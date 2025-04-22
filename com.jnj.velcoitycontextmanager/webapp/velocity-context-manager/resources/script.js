a5cd94fb708cc9baf6365ccf1a55bbd8f40b8c0d
Benish <benish.bm@intelizign.com>
2025-04-22 13:57:14 +0530

    Developed Prototype For Parameterization UI Below Mentioned changes Frontend Protoype Developed Admin Module Name in HiveModule.xml

--- com.jnj.velcoitycontextmanager/webapp/velocity-context-manager/resources/script.js
+++ com.jnj.velcoitycontextmanager/webapp/velocity-context-manager/resources/script.js
@@ -1,132 +1,115 @@
-var liveDocList;
-$(document).ready(function() {
-	$.ajax({
-		url: 'velocitycontextmanager?action=getLiveDocAttachedWithJson',
-		type: 'GET',
-		dataType: 'json',
-		success: function(response) {
-			if (response) {
-				liveDocList = response.liveDocAttachmentInfo;
-				liveDocList.forEach((docInfo, index) => {
-					const moduleName = docInfo.moduleName;
-					const fileName = docInfo.fileName;
+const sampleData = {
+	"Innomedic": [
+		{ moduleName: "Hazard RiskSheet", fileName: "riskParameter.json" },
+		{ moduleName: "Security Risksheet", fileName: "riskParameter.json" },
+		{ moduleName: "Harm RiskSheet", fileName: "riskParameter.json" },
+		{ moduleName: "Cause ModeSheet", fileName: "riskParameter.json" },
+		{ moduleName: "P1 Modesheet", fileName: "riskParameter.json" },
+		{ moduleName: "P2 Mode Sheet", fileName: "riskParameter.json" }
+	],
+	"Demo 123": [
+		{ moduleName: "Cause Mode RiskSheet", fileName: "riskParameter.json" },
+		{ moduleName: "Cause 2 Mode RiskSheet", fileName: "riskParameter.json" }
+	]
+};
 
-					let row = '<tr class="polarion-rpw-table-content-row">';
-					row += '<td style="width:40%">' + moduleName + '</td>';
-					row += '<td style="width:40%">' + fileName + '</td>';
-					row += '<td style="width:20%"><button class="edit-json-file-btn" data-index="' + index + '">Edit</button></td>';
-					row += '</tr>';
-
-					$('#json-file-body').append(row);
-					$(document).on('click', '.edit-json-file-btn', function() {
-						const index = $(this).data('index');
-						const doc = liveDocList[index];
-						console.log("Edit Its working");
-						//alert('Editing:\nModule: ' + doc.moduleName + '\nFile: ' + doc.fileName + '\nContent:\n' + doc.content);
-
-						$('#editor-module-name').text(doc.moduleName);
-						$('#editor-file-name').text(doc.fileName);
-						$('#json-textarea').val(doc.content);
-						//$('#json-editor').data('index', index).show();
-						$('#main-container').css('display', 'none');
-						$('#json-editor').css('display', 'block').data('index', index);
-
-					});
-
-				});
-			} else {
-				console.error('Failed to retrieve live document list');
-			}
-
-		},
-		error: function(error) {
-			console.error('Error occurred while fetching project list:', error);
+const jsonData = {
+	"columns": [
+		{
+			"headerGroup": "Final ranking",
+			"headerGroupCss": "headFinalRanking",
+			"headerCss": "headFinalRanking",
+			"header": "RPN",
+			"bindings": "rpnNew",
+			"id": "rpnNew",
+			"type": "int",
+			"cellRenderer": "rpn",
+			"filterable": true,
+			"width": 110,
+			"level": 3,
+			"collapseTo": true,
+			"formula": "commonRpnNew"
 		}
-	});
-});
+	],
+	"levels": [
+		{
+			"name": "Item",
+			"controlColumn": "item",
+			"zoomColumn": "item"
+		},
+		{
+			"name": "Failure mode",
+			"controlColumn": "failureMode",
+			"zoomColumn": "failureMode"
+		},
+		{
+			"name": "Cause",
+			"zoomColumn": "causes",
+			"controlColumn": "systemItemId"
+		}
+	],
+	"dataTypes": {
+		"risk": {
+			"type": "risk"
+		},
+		"task": {
+			"type": "task",
+			"role": "mitigates",
+			"name": "Task",
+			"zoomColumn": "taskTitle"
+		}
+	},
+	"sortBy": ["item", "failureMode", "causes"]
+};
 
-// Function to trigger the file input click when the button is clicked
-function triggerFileInput() {
-	const fileInput = document.getElementById('file-input');
-	fileInput.click();
+let currentEditingIndex = null;
+
+
+function openEditor(content, index) {
+	document.getElementById("parameter-div").style.display = "none";
+	document.getElementById("editor").style.display = "block";
+	document.getElementById("editContent").value = content;
+	currentEditingIndex = index;
+}
+
+function saveEditedContent() {
+	alert(`Parameter File Updated Sucessfully`);
+}
+
+function cancelEdit() {
+	document.getElementById("editor").style.display = "none";
+	document.getElementById("parameter-div").style.display = "block";
+	currentEditingIndex = null;
 }
 
 
-/*function editJsonFile() {
-	const index = $(this).data('index');
-	const doc = liveDocList[index];
-	alert('Editing:\nModule: ' + doc.moduleName + '\nFile: ' + doc.fileName + '\nContent:\n' + doc.content);
-	$('#editor-module-name').text(doc.moduleName);
-	$('#editor-file-name').text(doc.fileName);
-	$('#json-textarea').val(doc.content);
-	$('#json-editor').data('index', index).show();
+function handleApply() {
+	const tbody = document.getElementById("resultsBody");
+	tbody.innerHTML = ""; // Clear previous results
 
-}*/
+	Object.entries(sampleData).forEach(([moduleName, attachments]) => {
+		attachments.forEach(file => {
+			const row = document.createElement("tr");
 
+			row.innerHTML = `
+          <td>${file.moduleName}</td>
+           <td>Risk</td>
+          <td>${file.fileName}</td>
+          <td>
+            <button id="edit-btn" onclick="editFile()">Edit</button>
+          </td>
+        `;
 
-function closeJsonFile() {
-	$('#json-editor').css('display', 'none');
-	$('#main-container').css('display', 'block');
- }
-// On Save Click
-function saveJsonFile() {
-	const index = $('#json-editor').data('index');
-	const updatedContent = $('#json-textarea').val();
-	
-	liveDocList[index].content = updatedContent;
-
-	const updatedData = {
-		moduleName: liveDocList[index].moduleName,
-		fileName: liveDocList[index].fileName,
-		content: updatedContent
-	};
-
-	$.ajax({
-		url: 'velocitycontextmanager?action=updateLiveDocAttachment',
-		method: 'POST',
-		contentType: 'application/json',
-		data: JSON.stringify(updatedData),
-		success: function(response) {
-			alert('JSON updated successfully!');
-			$('#json-editor').hide();
-		    $('#main-container').css('display', 'block');
-		},
-		error: function(err) {
-			console.error('Error updating JSON:', err);
-			alert('Failed to update JSON.');
-		}
-	});
-}
-
-// Function to handle the file selection and upload
-function handleFileUpload(event) {
-	// Get the selected file
-	const file = event.target.files[0];
-	if (file) {
-		console.log('Selected file:', file.name);
-
-		// Create FormData object
-		const formData = new FormData();
-		formData.append('jsonFile', file);
-
-
-		$.ajax({
-			url: 'velocitycontextmanager?action=uploadJSONFile',
-			type: 'POST',
-			data: formData,
-			processData: false,
-			contentType: false,
-			success: function(response) {
-				if (response) {
-					console.log("Json File uploaded Sucessfully");
-				} else {
-					console.error('Issue on File Uploading');
-				}
-			},
-			error: function(error) {
-				console.error('Error occurred while fetching project list:', error);
-			}
+			tbody.appendChild(row);
 		});
-	}
+	});
+
+	document.getElementById("resultsTable").style.display = "block";
 }
+
+function editFile() {
+	    const jsonDataContent = JSON.stringify(jsonData, null, 2);
+		openEditor(jsonDataContent, 0);
+}
+
 
